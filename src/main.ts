8c962aaac6af32f328425234ec215d4ec168c8fc
Enoch Osarenren <osarenrenenoch@gmail.com>
2024-06-17 23:35:42 +0100

    debugging hbs issue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { graphqlMiddleware } from './common/middlewares/graphql.middleware';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
@@ -10,7 +9,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  
+
   await app.listen(3000);
 }
 bootstrap();
