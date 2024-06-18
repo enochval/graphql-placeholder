8b539eb4b19ceadef7bd47316e8da6ea077177e8
Enoch Osarenren <osarenrenenoch@gmail.com>
2024-06-18 13:57:27 +0100

    updated home and guide pages

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors({
+    origin: "*"
+  })
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('html');
